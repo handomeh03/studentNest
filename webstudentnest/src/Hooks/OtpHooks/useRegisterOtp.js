607ba4609b98c3afdd97688a1c08b223093b91f4
jameel hanomdeh <jameel.jalal8@gmail.com>
2025-11-28 02:12:44 +0300

    edit

--- webstudentnest/src/Hooks/OtpHooks/useRegisterOtp.js
+++ webstudentnest/src/Hooks/OtpHooks/useRegisterOtp.js
@@ -4,44 +4,35 @@ export function UseRegisterOtp() {
   const [success, setSuccess] = useState("");
   const [successFlag, setSuccessFlag] = useState(false);
   const [error, setError] = useState("");
-  const [registerOtpLoader, setregisterOtpLoader] = useState(false);
+  const [registerOtpLoader, setRegisterOtpLoader] = useState(false);
 
-  const sendOtp = async (email) => {
+  async function sendOtp(email) {
     try {
-      setregisterOtpLoader(true);
-
-      const res = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/v1/resend-otp`,
-        {
-          method: "POST",
-          body: JSON.stringify({ email }),
-          headers: { "Content-Type": "application/json" }
-        }
-      );
+      setRegisterOtpLoader(true);
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/resend-otp`, {
+        method: "POST",
+        body: JSON.stringify({ email }),
+        headers: { "Content-Type": "application/json" },
+      });
 
       const data = await res.json();
 
       if (res.ok) {
-        setSuccessFlag(true);
         setSuccess(data.message);
+        setSuccessFlag(true);
+        setError(""); 
+        
       } else {
-        throw new Error(data?.errors?.message || "Failed to send OTP");
+        throw new Error(data.error || data.errors?.message || "Failed to send OTP");
       }
-
     } catch (err) {
       setError(err.message);
+      setSuccess("");
+      setSuccessFlag(false);
     } finally {
-      setregisterOtpLoader(false);
+      setRegisterOtpLoader(false);
     }
-  };
+  }
 
-  return {
-    sendOtp,
-    registerOtpLoader,
-    error,
-    setError,
-    success,
-    successFlag,
-    setSuccessFlag
-  };
+  return { sendOtp, success, setSuccess, successFlag, setSuccessFlag, error, setError, registerOtpLoader };
 }
